658cf86eba059d3e2564b68d9c75d5cfe8c021a1
root <brantje@gmail.com>
2017-07-07 13:03:52 +0200

    Add example to retrive files

--- js/lib/tinymceNextcloudFileBrowser.js
+++ js/lib/tinymceNextcloudFileBrowser.js
@@ -1,4 +1,10 @@
 var NextCloudFileBrowserDialogue = function (field_name, url, type, win) {
+    var fileListUrl ='../files/ajax/list.php?dir=/';
+    function listDir(dir) {
+        $.get(fileListUrl, function (dirIndex) {
+            console.log('Dir index of' + dir +' ', dirIndex);
+        });
+    }
 
     var browser = $('<div id="mceNextcloudFileBrowser" />');
     browser.css('zIndex', 99999);
